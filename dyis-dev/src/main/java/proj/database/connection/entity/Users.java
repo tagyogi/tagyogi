7d1e28ccf8103ec49b7915e4ac0211740d14af40
ygy <yuns9534@naver.com>
2024-05-13 17:21:34 +0900

    커밋

---
+++ dyis-dev/src/main/java/proj/database/connection/entity/Users.java
@@ -0,0 +1,30 @@
+package proj.database.connection.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Users {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+	
+	private String nation;
+	private String lang;
+
+}
+
+
+
+
