2a43ab04e2d52fe97768d937dde70ea70c73575f
byun <byunsg@dongyangis.com>
2024-05-13 18:07:45 +0900

    변성균 테스트

--- dyis-dev/src/main/java/proj/web/SampleController.java
+++ dyis-dev/src/main/java/proj/web/SampleController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import proj.core.domain.JsonResult;
-import proj.service.LogProcService;
+import proj.service.LogProcService;  
 import proj.service.SampleService;
 
 import javax.servlet.http.HttpServletRequest;
